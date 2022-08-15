cf3176057d26bf5b8479cb1311bb482a5c45d105
Eric Ngo <quocdat32461997@yahoo.com>
2022-08-15 13:57:39 -0700

    Cleaned CopyGenerator and added unit-tests

--- models/copy_generator.py
+++ models/copy_generator.py
@@ -1,18 +1,19 @@
 from typing import List
 import torch
 import torch.nn.functional as F
-from torch.nn import Tensor
+from torch import Tensor
 
 
 class CopyGenerator(torch.nn.Module):
-    def __init__(self, input_dim: int, hidden_dims: List[int], num_heads: int = 12, dropout: float = 0.3):
+    def __init__(self, input_dim: int, hidden_dim_list: List[int], num_heads: int = 12, dropout: float = 0.3) -> None:
         """
         Args:
             input_dim: embedding dim
-            hidden_dims: dim-list of hidden layers (including output dimenstion)
+            hidden_dims: dim-list of hidden layers (including output dimenstion but not embedding dim)
             num_heads: number of attention heads. Default to to 12 as in BART
             dropout: float. Default to 0.3 as in Low-Resource Domain Adaptation for Compositional Task-Oriented Semantic Parsing
         """
+        super().__init__()
         self.copier = torch.nn.ModuleList([
             torch.nn.MultiheadAttention(num_heads=num_heads,
                                         embed_dim=input_dim,
@@ -21,13 +22,14 @@ class CopyGenerator(torch.nn.Module):
             torch.nn.Linear(input_dim, 1)])
 
         self.generator = torch.nn.ModuleList()
-        for index in range(len(hidden_dims)):
-            self.generator.append(torch.nn.Linear(input_dim, hidden_dims[index]))
-            if index < len(hidden_dims) - 1:
+        for index, hidden_dim in enumerate(hidden_dim_list):
+            self.generator.append(torch.nn.Linear(input_dim, hidden_dim))
+            if index < len(hidden_dim_list) - 1:
                 self.generator.append(torch.nn.ReLU())
                 self.generator.append(torch.nn.Dropout(dropout))
+            input_dim = hidden_dim
 
-    def forward(self, encoder_outputs: Tensor, decoder_outputs: Tensor):
+    def forward(self, encoder_outputs: Tensor, decoder_outputs: Tensor) -> Tensor:
         """
         Args:
             encoder_outputs: [batch_size, source_seq_len, embed_size]
