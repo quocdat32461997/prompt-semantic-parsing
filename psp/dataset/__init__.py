468b218d11e816cf24960b75203261561e9122d1
quocdat32461997 <quocdat32461997@yahoo.com>
2022-12-16 01:21:21 -0600

    Refined functionality of data-loader

--- psp/dataset/__init__.py
+++ psp/dataset/__init__.py
@@ -1,2 +1,2 @@
-from .data_loader import Tokenizer, LowResourceTOpv2Dataset, PromptTOPv2Dataset,  DataLoader
+from .data_loader import Tokenizer, LowResourceTOpv2Dataset, PromptTOPv2Dataset,  SMPDataLoader
 from .data_utils import read_and_merge
