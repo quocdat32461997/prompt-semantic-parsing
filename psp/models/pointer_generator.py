Fixed loss-computation
* Compute loss for outputs including ontology tokens + tokens from source-inputs (by index)
* Pointer-Generator: return ontology-tokens and indices of source-inputs

psp/models/pointer_generator.py
@@ -6,6 +6,40 @@ from torch import Tensor
 from psp.constants import RunMode
 from psp.models.model_utils import FFN
 
+class Generator(torch.nn.Module):
+    def __init__(self, input_dim: int, hidden_dim_list: List[int], dropout: float):
+        super(Generator, self).__init__()
+
+        self.ffn = FFN(input_dim=input_dim, hidden_dim_list=hidden_dim_list, dropout=dropout)
+
+    def forward(self, inputs):
+        """
+        Returns:
+            - outputs: probs of shape [batch_size, max_seq_len, ontology_vocab_size]
+        """
+        outputs = self.ffn(inputs)
+        outputs = F.softmax(outputs, dim=-1)
+
+        return outputs
+
+class CopyHead(torch.nn.Module):
+    def __init__(self, input_dim: int):
+        super(CopyHead, self).__init__()
+        self.linear = torch.nn.Linear(input_dim, 1)
+
+    def forward(self, inputs):
+        """
+        Args:
+            inputs: context ouputs from multi-head-attention module. 
+                Shape [batch_size, max_seq_len, embed_dim]
+        Returns:
+            outputs: scalar probs to copy from source inputs.
+                Shape [batch_size, max_seq_len, 1]
+        """
+        outputs = self.linear(inputs)
+        outputs = F.sigmoid(outputs)
+
+        return outputs
 
 class PointerGenerator(torch.nn.Module):
     def __init__(
@@ -19,7 +53,7 @@ class PointerGenerator(torch.nn.Module):
     ) -> None:
         """
         Args:
-            vocab_size: number of original vocabs + ontology vocabs
+            vocab_size: number of regular words + ontology vocabs
             ontology_vocab_ids: token-ids of ontology vocabs
             input_dim: embedding dim
             hidden_dims_list: dim-list of hidden layers (including output dimenstion but not embedding dim).
@@ -27,12 +61,13 @@ class PointerGenerator(torch.nn.Module):
             num_heads: number of attention heads. Default to to 12 as in BART
             dropout: float. Default to 0.3 as in Low-Resource Domain Adaptation for Compositional Task-Oriented Semantic Parsing
         """
-        super().__init__()
+        super(PointerGenerator, self).__init__()
         self.vocab_size: int = vocab_size
+
         self.ontology_vocab_ids: Tensor = torch.tensor(
             ontology_vocab_ids
-        )  # [ontology_vocab]
-
+        ).reshape([1, 1, -1])  # [1, 1, ontology_vocab]
+        
         # copier
         self.mha = torch.nn.MultiheadAttention(
             num_heads=num_heads,
@@ -40,10 +75,10 @@ class PointerGenerator(torch.nn.Module):
             dropout=dropout,
             batch_first=True,
         )
-        self.copy_head = torch.nn.Linear(input_dim, 1)
+        self.copy_head = CopyHead(input_dim=input_dim)
 
         # genrator
-        self.generator = FFN(
+        self.generator = Generator(
             input_dim=input_dim, hidden_dim_list=hidden_dim_list, dropout=dropout
         )
 
@@ -51,7 +86,10 @@ class PointerGenerator(torch.nn.Module):
         self,
         source_input_ids: Tensor,
         encoder_hidden_states: Tensor,
+        encoder_attn_mask: Tensor,
         decoder_hidden_states: Tensor,
+        run_mode: RunMode = RunMode.TRAIN,
+        device: str = None,
     ) -> Tensor:
         """
         Args:
@@ -59,68 +97,73 @@ class PointerGenerator(torch.nn.Module):
             encoder_hidden_states: [batch_size, source_seq_len, embed_size]
             decoder_hidden_states: [batch_size, max_seq_len or 1, embed_size]
         Returns:
-            vocab_probs: Tensor of shape [batch_size, max_seq_len or 1, vocab_size]
+            vocab_probs: Tensor of shape [batch_size, max_seq_len or 1, ontology_vocab_size + source_seq_len]
         """
 
-        # Generate ontology
-        # [batch_size, max_seq_len, ontology_vocab_size]
-        ontology_logits: Tensor = self.generator(decoder_hidden_states)
+        if not device:
+            device = source_input_ids.device
+        
+        # Generate probs to generate ontology tokens
+        # [batch_size, max_seq_len or 1, ontology_vocab_size]
+        ontology_probs: Tensor = self.generator(decoder_hidden_states)
 
         # Copy from source sequences
         # context_outputs: [batch_size, max_seq_len or 1, embed_dim]
-        # copy_source_probs: [batch_ssize, max_seq_len or 1, source_seq_len]
+        # copy_source_probs: [batch_size, max_seq_len or 1, source_seq_len]
         context_outputs, from_source_probs = self.mha(
             query=decoder_hidden_states,
             key=encoder_hidden_states,
+            key_padding_mask=encoder_attn_mask,
             value=encoder_hidden_states,
             need_weights=True,
         )
 
-        # copy_logits: [batch_size, max_seq_len or 1, 1]
-        copy_logits = self.copy_head(context_outputs)
-
-        # Get probs to generate onotlogies or copy from source
-        ontology_probs: Tensor = F.softmax(
-            ontology_logits, dim=-1
-        )  # [batch_size, max_seq_len or 1, ontology_vocab_size]
-        copy_probs: Tensor = torch.sigmoid(
-            copy_logits
-        )  # [batch_size, max_seq_len or 1, 1]
+        # Get probs to copy from source
+        # copy_probs: [batch_size, max_seq_len or 1, 1]
+        copy_probs = self.copy_head(context_outputs)
 
         # Get final probs
-        copy_source_probs = (
+        from_source_probs = (
             from_source_probs
             * copy_probs  # [batch_size, max_seq_len or 1, source_seq_len]
         )
-        generate_ontology_probs = ontology_probs * (
+        ontology_probs = ontology_probs * (
             1 - copy_probs
         )  # [batch_size, max_seq_len or 1, ontology_vocab_size]
 
         # Parse into full vocabs
         # [batch_size, max_seq_len or 1, vocab_size]
         vocab_probs: Tensor = torch.zeros(
-            list(copy_source_probs.shape[:2]) + [self.vocab_size],
-            dtype=copy_probs.dtype,
-        )
+            list(from_source_probs.shape[:2]) + [self.vocab_size],
+            dtype=copy_probs.dtype
+        ).to(device)
 
-        # [batch_size, 1, source_seq_len]
+        # Get indices to fill token-logits into vocab_probs
+        # [batch_size, max_seq_len, source_seq_len]
         source_input_ids = source_input_ids.unsqueeze(1)
         source_input_ids = torch.tile(source_input_ids, [1, copy_probs.shape[1], 1])
-
-        # [batch_size, max_seq_len or 1, source_seq_len]
-        vocab_probs = vocab_probs.scatter_add_(
-            dim=-1, index=source_input_ids, src=copy_source_probs
-        )
-
-        ontology_vocab_ids: Tensor = self.ontology_vocab_ids.reshape(
-            [1, 1, -1]
-        )  # [1, 1, ontology_vocab]
+        
         # [batch_size, max_seq_len or 1, ontology_vocab]
-        ontology_vocab_ids = torch.tile(
-            ontology_vocab_ids, list(vocab_probs.shape[:2]) + [1]
-        )
-        vocab_probs = vocab_probs.scatter_add_(
-            dim=-1, index=ontology_vocab_ids, src=generate_ontology_probs
-        )
+        ontology_vocab_ids: Tensor = torch.tile(
+            self.ontology_vocab_ids, list(vocab_probs.shape[:2]) + [1]
+        ).to(device)
 
+        if run_mode == RunMode.EVAL:
+            # [batch_size, max_seq_len, ontology_vocab_size + source_seq_len]
+            probs: Tensor = torch.concat([ontology_probs, from_source_probs], dim=-1)
+
+            # Apply softmax
+            probs = F.softmax(probs, dim=-1)
+
+            # Partition back to source-tokens and ontology-tokens
+            ontology_probs = probs[..., :ontology_vocab_ids.shape[-1]]
+            from_source_probs = probs[..., ontology_vocab_ids.shape[-1]:]
+
+        vocab_probs = vocab_probs.scatter_add(
+            dim=-1, index=source_input_ids, src=from_source_probs
+        ) 
+        vocab_probs = vocab_probs.scatter_add_(
+            dim=-1, index=ontology_vocab_ids, src=ontology_probs
+        )
+    
         return vocab_probs

train_low_resource.py
@@ -1,4 +1,5 @@
 import os
+import torch
 import argparse
 from torchtools.configs import Configs
 import pytorch_lightning as pl
@@ -13,6 +14,18 @@ from psp.dataset import (
     SMPDataLoader,
     LowResourceTOPDataset,
 )
+from typing import Dict
+
+def model_info(model: torch.nn.Module) -> None:
+    """Display total and trainable parameters of the given model. """
+    total_params = sum(
+	param.numel() for param in model.parameters()
+)
+
+    trainable_params = sum(
+	p.numel() for p in model.parameters() if p.requires_grad
+)
+    print('Total Params: {} \nTrainable Params: {}'.format(total_params, trainable_params))
 
 
 def setup(configs, **kwargs):
@@ -55,7 +68,7 @@ def setup(configs, **kwargs):
     # Built models
     print("Initiating model: {}.".format(configs.model_name))
     if configs.model_name == "Seq2SeqCopyPointer":
-        model = Seq2SeqCopyPointer(
+        core_model = Seq2SeqCopyPointer(
             pretrained=PRETRAINED_BART_MODEL,
             vocab_size=tokenizer.vocab_size,
             ontology_vocab_ids=tokenizer.ontology_vocab_ids,
@@ -71,11 +84,14 @@ def setup(configs, **kwargs):
         )
     else:
         raise ValueError("{} model is not a valid choice.".format(configs.model_name))
+    
+    # Information of mdoel
+    model_info(core_model)
 
     print("Initiating parser: {}.".format(configs.parser_name))
     if configs.parser_name == "LowResourceSemanticParser":
         model = LowResourceSemanticParser(
-            model=model,
+            model=core_model,
             lr=configs.lr,
             intent_id_list=tokenizer.intent_id_list,
             slot_id_list=tokenizer.slot_id_list,